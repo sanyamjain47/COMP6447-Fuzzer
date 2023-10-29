d267b00694b0173ce315ce031e4b773915df0ffa
SJ <sanyamjain47@gmail.com>
2023-10-28 20:14:36 -0400

    Added the most basic harness

---
+++ src/harness.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Run this multithreaded
+def run_binary_and_check_segfault(binary_path, input_data):
+    try:
+        process = subprocess.run([binary_path], input=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        
+        return False
+        
+    except subprocess.CalledProcessError as e:
+        print("An error occurred with exit code:", e.returncode)
+        return True
