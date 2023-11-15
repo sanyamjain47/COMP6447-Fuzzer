2dba6c94755e7066f1261a0803f326c6129998be
connor-isaias-white <connor.isaiaswhite@gmail.com>
2023-11-15 16:03:21 +1100

    lint fixes

--- src/library/payload_json.py
+++ src/library/payload_json.py
@@ -1,3 +1,4 @@
+"""Payload class for json"""
 import json
 from typing import Any, Union
 
@@ -5,16 +6,19 @@ from library import Payload
 from .helpers import get_dict_all_keys, update_nested_dict, get_nested_dict
 
 
-class PayloadJson(Payload): 
+class PayloadJson(Payload):
+    """Payload class for json will take in input of a string and allow for modifications"""
     def __init__(self, payload: str):
         payload = json.loads(payload)
         super().__init__(payload)
         self.keys = get_dict_all_keys(self.payload)
 
     def output(self) -> str:
+        """Produce Json output"""
         return json.dumps(self.payload)
 
     def set_field(self, field: Union[str, tuple[str]], val: Any):
+        """set field in json obj"""
         if isinstance(field, str):
             self.payload[field] = val
             self.keys.append((field,))
@@ -23,20 +27,24 @@ class PayloadJson(Payload):
             self.keys.append(field)
 
     def get_keys(self) -> list[tuple[str]]:
+        """set get list of tuples of keys to access object and nested objs"""
         return self.keys
 
     def get_val(self, field: Union[str, tuple]) -> Any:
+        """return the value at a key or nested key"""
         if isinstance(field, str):
             return self.payload[field]
         return get_nested_dict(self.payload, list(field))
 
     def get_keys_of_type(self, t: type) -> list[tuple[str]]:
+        """get all keys corresponding to types"""
         keys = []
         for k in self.keys:
             v = self.get_val(k)
-            if type(v) == t:
+            if isinstance(v, t):
                 keys.append(k)
         return keys
 
     def get_data(self) -> dict:
+        """get the raw dictionary data"""
         return self.payload
