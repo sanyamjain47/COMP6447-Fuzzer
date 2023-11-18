954e8c317dc89c5516d294c22a3bde2b732d3b2f
SJ <sanyamjain47@gmail.com>
2023-11-18 16:57:01 -0500

    Added jpeg fuzzing

--- src/jpeg_fuzzer.py
+++ src/jpeg_fuzzer.py
@@ -12,222 +12,137 @@ import struct
 ##########################
 ## JPEG SPECIFIC METHODS ##
 ##########################
+def flip_bits(byte_input):
+    ratio = 0.00001  # Example ratio, adjust as needed
+    length = len(byte_input) - 4
+    num_of_flips = int(length * ratio)
+    flip_array = [1, 2, 4, 8, 16, 32, 64, 128]
 
-def modify_header(jpeg_bytes, negative=False):
-    for i in range(2, 10):
-        if negative:
-            jpeg_bytes[i] = 0xFF + 1 + random.randint(-128, -1)
-        else:
-            jpeg_bytes[i] = random.randint(0, 255)
-    return jpeg_bytes
+    for _ in range(num_of_flips):
+        idx = random.randint(0, length)
+        mask = random.choice(flip_array)
+        byte_input[idx] ^= mask
 
+    return byte_input
 
-def insert_bytes(jpeg_bytes, variable_length=False, negative=False):
-    position = random.randint(2, len(jpeg_bytes) - 1)
-    length = random.randint(1, 100) if variable_length else random.randint(1, 5)
-    if negative:
-        random_bytes = bytearray((0xFF + 1 + random.randint(-128, -1)) for _ in range(length))
-    else:
-        random_bytes = bytearray(random.randint(0, 255) for _ in range(length))
-    return jpeg_bytes[:position] + random_bytes + jpeg_bytes[position:]
+def random_header_modification(byte_input):
+    length = len(byte_input) - 4
+    idx = random.randint(0, length)
+    byte_input[idx] ^= random.randint(0, 255)
+    return byte_input
 
-
-def remove_random_bytes(jpeg_bytes):
-    # Remove a sequence of bytes from a random position
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        length = random.randint(1, 5)
-        return jpeg_bytes[:position] + jpeg_bytes[position + length:]
-    return jpeg_bytes
-
-def flip_random_bits(jpeg_bytes):
-    # Flip a random bit in the file
-    byte_index = random.randint(0, len(jpeg_bytes) - 1)
-    bit_index = random.randint(0, 7)
-    byte_list = list(jpeg_bytes)
-    byte_list[byte_index] ^= 1 << bit_index
-    return bytearray(byte_list)
-
-def swap_segments(jpeg_bytes):
-    # Ensure the file is long enough to have multiple segments
-    if len(jpeg_bytes) > 20:
-        pos1 = random.randint(2, len(jpeg_bytes) // 2)
-        pos2 = random.randint(len(jpeg_bytes) // 2, len(jpeg_bytes) - 1)
-        segment1 = jpeg_bytes[pos1:pos1+5]
-        segment2 = jpeg_bytes[pos2:pos2+5]
-        jpeg_bytes[pos1:pos1+5], jpeg_bytes[pos2:pos2+5] = segment2, segment1
-    return jpeg_bytes
-
-def extend_segment_length(jpeg_bytes):
-    # Ensure there's enough data to work with
-    if len(jpeg_bytes) > 10:
-        # Choose a random position in the data
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        
-        # Calculate a new length (ensure it's within the bounds of the data)
-        new_length = random.randint(2, min(255, len(jpeg_bytes) - position - 1))
-
-        # Convert the new length to bytes
-        length_bytes = new_length.to_bytes(2, 'big')
-
-        # Replace the segment length in the data
-        # Make sure the slice you're replacing matches the size of length_bytes
-        jpeg_bytes[position:position + len(length_bytes)] = length_bytes
-
-    return jpeg_bytes
-
-def truncate_segment_length(jpeg_bytes):
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        length_bytes = jpeg_bytes[position:position+2]
-        length = max(1, int.from_bytes(length_bytes, 'big') - random.randint(1, 5))
-        jpeg_bytes[position:position+2] = length.to_bytes(2, 'big')
-    return jpeg_bytes
-
-def randomize_segment_order(jpeg_bytes):
-    # Assuming segments are of fixed length for simplicity
-    segment_length = 10
-    segments = [jpeg_bytes[i:i+segment_length] for i in range(0, len(jpeg_bytes), segment_length)]
-    random.shuffle(segments)
-    return bytearray().join(segments)
-
-def inject_format_strings(jpeg_bytes):
-    format_str = b'%s%p%x%n'
-    position = random.randint(2, len(jpeg_bytes) - 1)
-    return jpeg_bytes[:position] + format_str + jpeg_bytes[position:]
-
-def duplicate_segment(jpeg_bytes):
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        segment = jpeg_bytes[position:position+5]
-        return jpeg_bytes[:position] + segment + segment + jpeg_bytes[position:]
-    return jpeg_bytes
-
-def reverse_segment(jpeg_bytes):
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        segment = jpeg_bytes[position:position+5]
-        jpeg_bytes[position:position+5] = segment[::-1]
-    return jpeg_bytes
+def insert_random_marker(byte_input):
+    markers = [0xd8, 0xe0, 0xe1]  # Example markers, adjust as needed
+    position = random.randint(2, len(byte_input) - 2)
+    marker = random.choice(markers)
+    return byte_input[:position] + bytes([marker]) + byte_input[position:]
 
 
-def inject_nop_sled(jpeg_bytes):
-    nop_sled = b'\x90' * random.randint(10, 100)
-    position = random.randint(2, len(jpeg_bytes) - 1)
-    return jpeg_bytes[:position] + nop_sled + jpeg_bytes[position:]
+def swap_segments(byte_input):
+    seg1_start = random.randint(0, len(byte_input) // 2)
+    seg1_end = seg1_start + random.randint(2, 20)
+    seg2_start = random.randint(len(byte_input) // 2, len(byte_input) - 20)
+    seg2_end = seg2_start + random.randint(2, 20)
 
+    byte_input[seg1_start:seg1_end], byte_input[seg2_start:seg2_end] = \
+        byte_input[seg2_start:seg2_end], byte_input[seg1_start:seg1_end]
 
-def negative_length_segment(jpeg_bytes):
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        negative_length = -random.randint(1, 100)
-        jpeg_bytes[position:position+2] = negative_length.to_bytes(2, byteorder='big', signed=True)
-    return jpeg_bytes
+    return byte_input
 
-def negative_byte_injection(jpeg_bytes):
-    # Insert a sequence of 'negative' bytes at a random position
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        length = random.randint(1, 5)
-        negative_bytes = bytearray((0xFF + 1 + random.randint(-128, -1)) for _ in range(length))
-        return jpeg_bytes[:position] + negative_bytes + jpeg_bytes[position + length:]
-    return jpeg_bytes
+def random_block_shuffle(byte_input):
+    block_size = 64  # 8x8 block
+    num_blocks = len(byte_input) // block_size
+    blocks = [byte_input[i * block_size:(i + 1) * block_size] for i in range(num_blocks)]
+    random.shuffle(blocks)
+    return b''.join(blocks)
 
-def negative_offset_in_headers(jpeg_bytes):
-    for i in range(2, 10):
-        # Convert negative value to unsigned byte representation
-        jpeg_bytes[i] = 0xFF + 1 + random.randint(-128, -1)
-    return jpeg_bytes
+def random_metadata_insertion(byte_input):
+    exif_header = b'\xff\xe1' + struct.pack('>H', random.randint(2, 100))
+    random_metadata = bytes([random.randint(0, 255) for _ in range(random.randint(1, 20))])
+    return exif_header + random_metadata + byte_input
 
-def negative_value_flip(jpeg_bytes):
-    byte_index = random.randint(0, len(jpeg_bytes) - 1)
-    # Convert negative value to unsigned byte representation
-    jpeg_bytes[byte_index] = 0xFF + 1 + random.randint(-128, -1)
-    return jpeg_bytes
-
-
-def reverse_segment_negative(jpeg_bytes):
-    if len(jpeg_bytes) > 10:
-        position = random.randint(2, len(jpeg_bytes) - 5)
-        segment = jpeg_bytes[position:position+5]
-        segment_with_negatives = bytearray((0xFF + 1 + random.randint(-128, -1)) if random.random() < 0.5 else b for b in segment)
-        jpeg_bytes[position:position+5] = segment_with_negatives[::-1]
-    return jpeg_bytes
-
-
-
-
-def generate_jpeg_fuzzed_output(jpeg_bytes, fuzzed_queue, binary_path):
-    jpeg_mutators = [
-        modify_header,
-        insert_bytes,
-        remove_random_bytes,
-        flip_random_bits,
-        swap_segments,
-        extend_segment_length,
-        truncate_segment_length,
-        randomize_segment_order,
-        inject_format_strings,
-        duplicate_segment,
-        reverse_segment,
-        inject_nop_sled,
-        negative_length_segment,
-        negative_byte_injection,
-        negative_offset_in_headers,
-        negative_value_flip,
-        reverse_segment_negative
+def mutate_magic(data):
+    values = [
+        (1, 0xff), (1, 0x7f), (1, 0),
+        (2, 0xffff), (2, 0),
+        (4, 0xffffffff), (4, 0), (4, 0x80000000), (4, 0x40000000), (4, 0x7fffffff)
     ]
+    length = len(data) - 8
+    idx = randint(0, length)
+    n_size, n = random.choice(values)
 
-    #TODO Remove this - Testing code only
+    data[idx:idx+n_size] = n.to_bytes(n_size, 'little')
+    return data
+
+def flip_ratio_bits(data, ratio):
+    length = len(data) - 4
+    num_of_flips = int(length * ratio)
+    flip_array = [1, 2, 4, 8, 16, 32, 64, 128]
+
+    for _ in range(num_of_flips):
+        idx = randint(0, length)
+        mask = random.choice(flip_array)
+        data[idx] ^= mask
+
+    return data
+
+def flip_random_bit(data):
+    length = len(data) - 4
+    idx = randint(0, length)
+    flip_array = [1, 2, 4, 8, 16, 32, 64, 128]
+    mask = random.choice(flip_array)
+    data[idx] ^= mask
+
+    return data
+
+def flip_random_byte(data):
+    length = len(data) - 4
+    idx = randint(0, length)
+    data[idx] ^= 0xff
+
+    return data
+
+
+# Fuzzing generator
+def generator_jpeg(mutator_queue, input_jpeg, fuzzed_queue):
+    while not mutator_queue.empty():
+        mutator_combination = mutator_queue.get()
+        fuzzed_output = input_jpeg[:]
+        for mutator in mutator_combination:
+            fuzzed_output = mutator(fuzzed_output)
+        fuzzed_queue.put({"input": fuzzed_output, "mutator": mutator_combination})
+
+# Thread management for fuzzing
+def multi_threaded_generator_jpeg(mutator_queue, input_jpeg, fuzzed_queue, num_threads=5):
+    threads = [threading.Thread(target=generator_jpeg, args=(mutator_queue, input_jpeg, fuzzed_queue)) for _ in range(num_threads)]
+    for thread in threads:
+        thread.start()
+    return threads
+
+def multi_threaded_harness(binary_path, fuzzed_queue, num_threads=5):
+    threads = [threading.Thread(target=run_binary_and_check_segfault, args=(binary_path, fuzzed_queue)) for _ in range(num_threads)]
+    for thread in threads:
+        thread.start()
+    return threads
+
+# Main fuzzing function
+def generate_jpeg_fuzzed_output(jpeg_path, fuzzed_queue, binary_path):
     with open(jpeg_path, 'rb') as file:
         jpeg_bytes = bytearray(file.read())
 
+    jpeg_mutators = [flip_bits, random_header_modification, insert_random_marker, swap_segments, random_block_shuffle, random_metadata_insertion]
     all_possible_mutations = Queue()
-    for r in range(1, len(jpeg_mutators) + 1):
-        for mutator_combination in itertools.combinations(jpeg_mutators, r):
-            all_possible_mutations.put(mutator_combination)
-    print(all_possible_mutations.qsize())
-    # Start generator threads
-    generator_threads = multi_threaded_generator_jpeg(all_possible_mutations, jpeg_bytes, fuzzed_queue, num_threads=20)
 
-    # Start harness threads
+    for _ in range(20):
+        for r in range(1, len(jpeg_mutators) + 1):
+            for mutator_combination in itertools.combinations(jpeg_mutators, r):
+                all_possible_mutations.put(mutator_combination)
+
+    generator_threads = multi_threaded_generator_jpeg(all_possible_mutations, jpeg_bytes, fuzzed_queue, num_threads=20)
     harness_threads = multi_threaded_harness(binary_path, fuzzed_queue, num_threads=20)
 
-    # Wait for all generator and harness threads to complete
     for thread in generator_threads + harness_threads:
         thread.join()
-
-def multi_threaded_generator_jpeg(mutator_queue, input_jpeg, fuzzed_queue, num_threads=5):
-    threads = []
-    def thread_target():
-        generator_jpeg(mutator_queue, input_jpeg, fuzzed_queue)
-    for _ in range(num_threads):
-        thread = threading.Thread(target=thread_target)
-        threads.append(thread)
-        thread.start()
-    return threads
-
-def generator_jpeg(mutator_queue, input_jpeg, fuzzed_queue):
-    while True:
-        if not mutator_queue.empty():
-            mutator_combination = mutator_queue.get()
-            fuzzed_output = input_jpeg[:]
-            for mutator in mutator_combination:
-                fuzzed_output = mutator(fuzzed_output)
-            fuzzed_queue.put({"input": fuzzed_output, "mutator": mutator_combination})
-        else:
-            print("done")
-            return
-
-def multi_threaded_harness(binary_path, fuzzed_queue, num_threads=5):
-    threads = []
-    def thread_target():
-        run_binary_and_check_segfault(binary_path, fuzzed_queue)
-    for _ in range(num_threads):
-        thread = threading.Thread(target=thread_target)
-        threads.append(thread)
-        thread.start()
-    return threads
 
 if __name__ == "__main__":
     q = Queue()
