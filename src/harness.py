4aba75d666e557640ee3386ca6ea42333f4cfc1f
SJ <sanyamjain47@gmail.com>
2023-11-18 17:26:37 -0500

    Adding new harness

--- src/harness.py
+++ src/harness.py
@@ -1,37 +1,59 @@
-from pwn import *
-from queue import Queue
-import sys
 import subprocess
+import time
+import sys
+import threading
 
+# Shared flag to signal termination
+terminate_flag = threading.Event()
 
-def run_binary_and_check_segfault(binary_path, q):
+def run_binary_bytes(binary_path, q):
     start_time = time.time()
     time_limit = 150  # 150 seconds
-    
-    while time.time() - start_time < time_limit:
+
+    while not terminate_flag.is_set() and time.time() - start_time < time_limit:
         if not q.empty():
             input_data = q.get()['input']
             try:
-                process = subprocess.run([binary_path], input=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+                trace_cmd = f"ltrace -o trace_output.txt {binary_path}"
+                process = subprocess.run(trace_cmd, input=input_data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
             except subprocess.CalledProcessError as e:
-                if e.returncode == -11:
+                if e.returncode in [-11, -6]:
                     print(f"An error occurred with exit code: {e.returncode}")
-                    generate_report(input_data,e)
-                    print(f"The input the caused the error has been put in bad.txt")
+                    terminate_flag.set()
                     sys.exit()
-                if e.returncode == -6:
-                    print(f"An error occurred with exit code: {e.returncode}")
-                    generate_report(input_data,e)
-                    print(f"The input the caused the error has been put in bad.txt")
-                    sys.exit()
-
+                elif e.returncode < 0 and e.returncode != -2:
+                    print(f"Exit code encountered: {e.returncode}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
         else:
-            # Sleep for a short duration to avoid busy-waiting
             time.sleep(5)
-        
 
-def generate_report(s: str, e):
-    with open('bad.txt', 'a') as f:
-        f.write("Input causing the error:\n{}\n".format(s))
-        f.write("Error code: {}\n".format(e.returncode))
-        f.write("=" * 40 + "\n")  # Add a separator for readability
+    print("Exiting run_binary_bytes")
+
+
+# Ensure that threads check the terminate_flag regularly and terminate if it's set
+
+
+def run_binary_string(binary_path, q):
+    start_time = time.time()
+    time_limit = 150  # 150 seconds
+
+    while not terminate_flag.is_set() and time.time() - start_time < time_limit:
+        if not q.empty():
+            input_data = q.get()['input']
+            try:
+                input_data = input_data.encode('utf-8')
+                process = subprocess.run([binary_path], input=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            except subprocess.CalledProcessError as e:
+                if e.returncode in [-11, -6]:
+                    print(f"An error occurred with exit code: {e.returncode}")
+                    terminate_flag.set()
+                    sys.exit()
+                elif e.returncode < 0 and e.returncode != -2:
+                    print(f"Exit code encountered: {e.returncode}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+        else:
+            time.sleep(5)
+            print(5)
+
