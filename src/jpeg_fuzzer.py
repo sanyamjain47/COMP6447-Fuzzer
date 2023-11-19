ac68aa68ca41d38505a5839421d5b9b31126f38b
SJ <sanyamjain47@gmail.com>
2023-11-18 22:43:08 -0500

    Added some jpeg specific mutations

--- src/jpeg_fuzzer.py
+++ src/jpeg_fuzzer.py
@@ -131,23 +131,154 @@ def flipRatioBytes( data):
     
     return data
 
+def fuzz_soi(data):
+    # Add random data after the SOI marker
+    if len(data) >= 2:
+        extra_data = bytearray(random.getrandbits(8) for _ in range(1000))  # Add 1000 bytes
+        data = data[:2] + extra_data + data[2:]
+    return data
+
+
+def fuzz_quantization_tables(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xDB:
+            # Inject a large quantization table
+            large_table = bytearray(random.getrandbits(8) for _ in range(2000))  # 2000 bytes
+            data = data[:i + 4] + large_table + data[i + 4:]
+            i += 2004
+        else:
+            i += 1
+    return data
+
+def fuzz_sof(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xC0:
+            extra_data = bytearray(random.getrandbits(8) for _ in range(1000))  # Add 1000 bytes
+            data = data[:i + 4] + extra_data + data[i + 4:]
+            i += 1004
+        else:
+            i += 1
+    return data
+
+
+def fuzz_huffman_tables(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xC4:
+            large_table = bytearray(random.getrandbits(8) for _ in range(2000))  # 2000 bytes
+            data = data[:i + 4] + large_table + data[i + 4:]
+            i += 2004
+        else:
+            i += 1
+    return data
+
+
+def fuzz_image_data(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xDA:
+            extra_data = bytearray(random.getrandbits(8) for _ in range(5000))  # Add 5000 bytes
+            data = data[:i + 4] + extra_data + data[i + 4:]
+            break
+        else:
+            i += 1
+    return data
+def fuzz_dqt(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xDB:
+            large_table = bytearray(random.getrandbits(8) for _ in range(2000))  # 2000 bytes
+            data = data[:i + 4] + large_table + data[i + 4:]
+            i += 2004
+        else:
+            i += 1
+    return data
+
+def fuzz_dri(data):
+    i = 0
+    while i < len(data) - 2:
+        if data[i] == 0xFF and data[i + 1] == 0xDD:
+            extra_data = bytearray(random.getrandbits(8) for _ in range(1000))  # Add 1000 bytes
+            data = data[:i + 4] + extra_data + data[i + 4:]
+            i += 1004
+        else:
+            i += 1
+    return data
+
+
+def fuzz_jpeg_markers(data):
+    markers_to_fuzz = [0xC0, 0xC4, 0xDA, 0xDB, 0xDD]  # SOF, DHT, Scan, DQT, DRI
+    for i in range(len(data) - 1):
+        if data[i] == 0xFF and data[i + 1] in markers_to_fuzz:
+            data[i + 1] = random.choice(markers_to_fuzz)  # Swap with another marker
+    return data
+
+def fuzz_length_fields(data):
+    for i in range(len(data) - 3):
+        if data[i] == 0xFF:
+            # Randomly change the length field
+            length = (data[i + 2] << 8) | data[i + 3]
+            fuzzed_length = random.randint(0, 0xFFFF)
+            data[i + 2] = (fuzzed_length >> 8) & 0xFF
+            data[i + 3] = fuzzed_length & 0xFF
+    return data
+
+def fuzz_exif_data(data):
+    exif_marker = b'\xFF\xE1'
+    # Insert a random EXIF marker
+    insert_position = random.randint(2, len(data) - 1)
+    data = data[:insert_position] + exif_marker + b'\x00\x10' + bytearray(random.getrandbits(8) for _ in range(16)) + data[insert_position:]
+    return data
+
+def fuzz_overlapping_segments(data):
+    i = 0
+    while i < len(data) - 1:
+        if data[i] == 0xFF:
+            # Check if there are enough bytes left for the length field
+            if i + 3 < len(data):
+                length = (data[i + 2] << 8) | data[i + 3]
+                if length > 1 and i + 2 + length < len(data):
+                    cut_length = random.randint(1, length)
+                    data = data[:i + 2 + cut_length] + data[i + 2 + length:]
+                i += 2 + length
+            else:
+                break  # Not enough bytes left for a complete segment
+        else:
+            i += 1
+    return data
+
+
+
+
 def generate_jpeg_fuzzed_output(df, fuzzed_queue, binary_path, output_queue):
     jpeg_mutator = [
         flipRandomBit,
         flipRandomByte,
         flipRatioBits,
         flipRatioBytes,
-        mutate_magic
+        mutate_magic,
+        fuzz_soi,
+        fuzz_quantization_tables,
+        fuzz_sof,
+        fuzz_huffman_tables,
+        fuzz_image_data,
+        fuzz_dqt,
+        fuzz_dri,
+        fuzz_jpeg_markers,
+        fuzz_length_fields,
+        fuzz_exif_data,
+        fuzz_overlapping_segments,
     ]
     all_possible_mutations = Queue()
     list_all_possible_mutations = []
     
-    for _ in range(100):
-        for r in range(1, len(jpeg_mutator) + 1):
-            for mutator_combination in itertools.combinations(jpeg_mutator, r):
-                all_possible_mutations.put(mutator_combination)
-                list_all_possible_mutations.append(mutator_combination)
-        
+    
+    for r in range(1, len(jpeg_mutator) + 1):
+        for mutator_combination in itertools.combinations(jpeg_mutator, r):
+            all_possible_mutations.put(mutator_combination)
+            list_all_possible_mutations.append(mutator_combination)
     # Start generator threads
     generator_threads = multi_threaded_generator_csv(all_possible_mutations, df, fuzzed_queue, num_threads=20)
 
